15f24b2da19fe633e95d70dd0556492e57f62e9f
Aditia Nuzlar <nuzlar_addit93@yahoo.co.id>
2021-08-28 12:45:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,38 +1,45 @@
 <!DOCTYPE html>
 <html>
+<?php
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+function content($id)
+{
+    $id_menu = base64_decode($id);
+    if ($id_menu == 1) {
+        $file = "latihan1.php";
+    } elseif ($id_menu == 2) {
+        $file = "latihan5.php";
+    } elseif ($id_menu == 3) {
+        $file = "latihan9.php";
+    } elseif ($id_menu == 4) {
+        $file = "latihan13.php";
+    } elseif ($id_menu == "") {
+        $file = "welcome.php";
+    }
+    return $file;
+}
+?>
 <body>
-<table width="100%" height="100%" border="1">
-    <tr height="5%">
-        <td align="center">
-            <a href="index.php?id=<?php echo base64_encode(1); ?>">Mahasiswa</a>
-            <a href="index.php?id=<?php echo base64_encode(2); ?>">Dosen</a>
-            <a href="index.php?id=<?php echo base64_encode(3); ?>">Matakuliah</a>
-            <a href="index.php?id=<?php echo base64_encode(4); ?>">Nilai</a>
-        </td>
-    </tr>
-    <tr height="95%">
-        <td valign="top" align="center">
-            <?php
-                $id_menu =base64_decode($_GET["id"]);
-                if($id_menu==1)
-                {
-                    include('latihan1.php');
-                }
-                elseif($id_menu==2)
-                {
-                    include('latihan5.php');
-                }
-                elseif($id_menu==3)
-                {
-                    include('latihan9.php');
-                }
-                elseif($id_menu==4)
-                {
-                    include('latihan13.php');
-                }
-            ?>
-        </td>
-    </tr>
-</table>
+    <table width="100%" height="100%" border="1">
+        <tr height="5%">
+            <td align="center">
+                <a href="index.php?id=<?php echo base64_encode(1); ?>">Mahasiswa</a>
+                <a href="index.php?id=<?php echo base64_encode(2); ?>">Dosen</a>
+                <a href="index.php?id=<?php echo base64_encode(3); ?>">Matakuliah</a>
+                <a href="index.php?id=<?php echo base64_encode(4); ?>">Nilai</a>
+            </td>
+        </tr>
+        <tr height="95%">
+            <td valign="top" align="center">
+                <?php
+                $file = content($_GET['id']);
+                include "$file";
+                ?>
+                <br><br>
+            </td>
+        </tr>
+    </table>
 </body>
+
 </html>
